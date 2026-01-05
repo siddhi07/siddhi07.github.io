97f3a3a15b8616da68d8d46df7a8230f9166a51c
Siddhi Nirmale <63958658+siddhi07@users.noreply.github.com>
2026-01-04 22:08:41 -0500

    Update typing.js

--- assets/typing.js
+++ assets/typing.js
@@ -1,5 +1,6 @@
 const text = "Hi, I’m Siddhi.";
 const target = document.getElementById("typed-text");
+const cursor = document.querySelector(".cursor");
 
 let index = 0;
 
@@ -7,7 +8,12 @@ function type() {
   if (index < text.length) {
     target.textContent += text.charAt(index);
     index++;
-    setTimeout(type, 80); // typing speed
+    setTimeout(type, 80);
+  } else {
+    // let cursor blink a bit, then hide
+    setTimeout(() => {
+      cursor.classList.add("hide");
+    }, 1200);
   }
 }
 
